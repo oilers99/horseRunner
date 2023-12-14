47cd755d063d2070416b09e1a6f9fad7058e0365
oliers <gdss@yandex.ru>
2023-12-14 12:54:57 +0300

    add class StartWindows

--- visual/visual.py
+++ visual/visual.py
@@ -4,7 +4,7 @@ import visual_settings
 
 class Game:
     """
-
+    основное окно игры
     """
     def __init__(self):
         pygame.init()
@@ -18,21 +18,23 @@ class Game:
         settings windows
         """
         """create wind and param: SCALED"""
-        self.screen = pygame.display.set_mode(visual_settings.SIZE_WINDOWS, pygame.SCALED)
+        self.screen = pygame.display.set_mode(visual_settings.SIZE_MAIN_WINDOWS)
         """name wind"""
         pygame.display.set_caption(visual_settings.NAME_WINDOWS)
         """icon wind"""
         # pygame.display.set_icon(visual_settings.ICON_WINDOWS) # вставить икону в переменную
+        """Font"""
+        self.font = visual_settings.FONT
 
     def start_new_game(self):
         """
-
+        Пока что только перевенная для последующего вывода победителя
         """
         self.winner = []
 
     def launch(self):
         """
-
+        цикл обновления
         """
         while self.running:
             self.game_events()
@@ -40,7 +42,7 @@ class Game:
 
     def game_events(self):
         """
-
+        отслеживание нажатия клавиш
         """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -55,12 +57,14 @@ class Game:
         if key == pygame.K_ESCAPE:
             self.running = False
 
+        elif key == pygame.K_BACKSPACE:
+            pass
+
         elif 49 <= key <= 57:
             pass
 
         elif key == pygame.K_RETURN:
             pass
-
 
     def __del__(self):
         """
@@ -69,7 +73,68 @@ class Game:
         pygame.quit()
 
 
-gam = Game()
-gam.launch()
+class StartWindows:
+    """
+    Стартовое окно
+    после ввода количества играков передает в Game
+    и закрывается
+    """
+    def __init__(self):
+        pygame.init()
+        self.set_up_start_canvas()
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def set_up_start_canvas(self):
+        """
+        Параметры стартового окна
+        """
+        """create start wind"""
+        self.screen = pygame.display.set_mode(visual_settings.SIZE_START_WINDOWS)
+        """name start wind"""
+        pygame.display.set_caption(visual_settings.NAME_START_WINDOWS)
+        """icon start wind"""
+        # pygame.display.set_icon(visual_settings.ICON_START_WINDOWS) # вставить икону в переменную
+        """Font start wind"""
+        self.font = visual_settings.FONT_START_WINDOWS
+
+    def launch_start_windows(self):
+        """
+        цикл обновления
+        """
+        while self.running:
+            self.game_events_start_windows()
+            self.clock.tick(visual_settings.FPS_VAR)
+
+    def game_events_start_windows(self):
+        """
+        отслеживание нажатия клавиш
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                self.handle_kay_press_for_start_windows(event.key)
+
+    def handle_kay_press_for_start_windows(self, key):
+        """
+        обработка нажатия клавиш
+        """
+        if key == pygame.K_ESCAPE:
+            self.running = False
+
+        elif key == pygame.K_BACKSPACE:
+            pass
+
+        elif 49 <= key <= 57:
+            pass
+
+        elif key == pygame.K_RETURN:
+            """запуск основного окна, закрытие стартового окна"""
+            game = Game()
+            game.launch()
+            self.running = False
 
 
+start_windows = StartWindows()
+start_windows.launch_start_windows()
--- visual/visual_settings.py
+++ visual/visual_settings.py
@@ -1,7 +1,11 @@
 import pygame
 pygame.init()
-"""Размер окна"""
-SIZE_WINDOWS = (1600, 800)
+
+"""
+Параметры главного окна class Game
+"""
+"""Размер главного окна"""
+SIZE_MAIN_WINDOWS = (1600, 800)
 """Заголовок окна"""
 NAME_WINDOWS = "_HorseRacer_"
 """Икона окна"""
@@ -9,4 +13,16 @@ ICON_WINDOWS = None
 """Шрифт и размер для теста"""
 FONT = pygame.font.SysFont('arial', 32)
 """FPS var"""
-FPS_VAR = 60
+FPS_VAR = 30
+
+"""
+Параметры стартового окна class StartWindows
+"""
+"""Размер стартового окна"""
+SIZE_START_WINDOWS = (400, 200)
+"""Заголовок окна"""
+NAME_START_WINDOWS = "_HorseRacer_"
+"""Икона окна"""
+ICON_START_WINDOWS = None
+"""Шрифт и размер для теста"""
+FONT_START_WINDOWS = pygame.font.SysFont('arial', 32)
